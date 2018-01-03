3992fd581d54fab42cf6b85260d128cd5b655e27
binaribd <33418419+binaribd@users.noreply.github.com>
2018-01-04 05:26:25 +0600

    update

--- js/custom.js
+++ js/custom.js
@@ -122,3 +122,19 @@ $("#cssmenu").menumaker({
 
 
 
+
+
+
+/* sticky js Start Here */
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+/* sticky js ends Here */
+
