347654c8097b47e6bd00e55f7705f1774cdc50fd
binaribd <33418419+binaribd@users.noreply.github.com>
2018-01-06 20:04:01 +0600

    update

--- js/custom.js
+++ js/custom.js
@@ -138,3 +138,43 @@ function myFunction() {
 }
 /* sticky js ends Here */
 
+function validateForm() {
+if (isEmpty(document.getElementById('data_4').value.trim())) {
+alert('First name is required!');
+return false;
+}
+if (isEmpty(document.getElementById('data_5').value.trim())) {
+alert('Last name is required!');
+return false;
+}
+if (isEmpty(document.getElementById('data_6').value.trim())) {
+alert('Email is required!');
+return false;
+}
+if (!validateEmail(document.getElementById('data_6').value.trim())) {
+alert('Email must be a valid email address!');
+return false;
+}
+if (isEmpty(document.getElementById('data_7').value.trim())) {
+alert('Phone is required!');
+return false;
+}
+if (isEmpty(document.getElementById('data_8').value.trim())) {
+alert('Company is required!');
+return false;
+}
+if (isEmpty(document.getElementById('data_10').value.trim())) {
+alert('Address is required!');
+return false;
+}
+if (isEmpty(document.getElementById('data_11').value.trim())) {
+alert('City is required!');
+return false;
+}
+return true;
+}
+function isEmpty(str) { return (str.length === 0 || !str.trim()); }
+function validateEmail(email) {
+var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,15}(?:\.[a-z]{2})?)$/i;
+return isEmpty(email) || re.test(email);
+}
